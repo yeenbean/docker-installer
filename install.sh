a9602636d928b7687ee73fed43a16d26f9a0cbfd
Joshua <7747985+yeenbean@users.noreply.github.com>
2023-07-07 12:17:18 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# tbd: copy/paste comment for console/ssh.
+
+
+init() {
+  set -euo pipefail # strict mode
+  #set -x            # uncomment when debugging your script
+  IFS=$'\n\t'       # IFS restricted to newline and tab
+}
+
+
+main() {
+  init # run init function
+
+  # setup repository dependencies (HTTPS repo)
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl gnupg
+
+  # add docker's GPG key
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+  # setup repository
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  # install docker
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+
+# make sure bash 4 is being used and run the script, exit otherwise
+# this prevents people from running the script with an unsupported interpreter
+if [ -z "${BASH_VERSINFO}" ] || [ -z "${BASH_VERSINFO[0]}" ] || [ ${BASH_VERSINFO[0]} -lt 4 ]
+then
+  echo "This script requires Bash version 4 or newer."
+  exit 1
+else
+  main
+fi
