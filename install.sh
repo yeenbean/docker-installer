a05b817e69d1087f92f0b56ec1a7de8651a70725
Joshua <7747985+yeenbean@users.noreply.github.com>
2023-07-11 11:24:10 -0700

    Add flag to automatically install everything

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ main() {
 
   # install docker
   sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 
